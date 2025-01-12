909aac4535b35163c371989681167f275d68bc3c
kayleigh222 <kayleighsleath@gmail.com>
2025-01-12 05:40:35 +0000

    added m~i~stin~g ~~~~~system first draf~t~ t~o~ c~o~de

--- image_capture_code.py
+++ image_capture_code.py
@@ -3,17 +3,32 @@ import subprocess
 import time
 import boto3
 import os
+import RPi.GPIO as GPIO
 
-# captures an image every X seconds and uploads it to AWS storage bucket
+# turns on the watering system every X seconds, then captures an image and uploads it to AWS storage bucket
 
 
 # aws s3 setup
 BUCKET_NAME = 'grobot-data-bucket'
 s3_client = boto3.client('s3')
 S3_FOLDER = 'microgreen-images/'
+IMAGE_FOLDER = '/home/raspberrypi/Desktop/grobot/'
 
-IMAGE_FOLDER = '/home/raspberrypi/Desktop/grobot/' 
+# compressore relay setup
+RELAY_PIN = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
 
+TIME_BETWEEN = 120 # seconds to wait between watering/image capture
+WATERING_TIME = 60 # seconds to turn on misting system
+
+# -----------MISTING SYSTEM FNS-----------
+def water():
+    GPIO.output(RELAY_PIN, GPIO.HIGH)
+    time.sleep(WATERING_TIME)
+    GPIO.output(RELAY_PIN, GPIO.LOW)
+
+# ----------GROWTH IMAGE FNS--------------
 def capture_image():
 #     capture image with rpicam
     timestamp = datetime.now().strftime('%y%m%d_%H%M%S')
@@ -38,12 +53,16 @@ def upload_to_s3(file_path):
 
 def main():
     while True:
+#         TURN ON MISTING SYSTEM
+        water()
+#         CAPTURE AND UPLOAD GROWTH IMAGE
         image_path = capture_image()
         if image_path:
-            upload_to_s3(image_path)
+#             upload_to_s3(image_path)
+# ^ above line temporarily commented out so not using free aws limits while testing growing
 #             clean up local image
             os.remove(image_path) 
-        time.sleep(60)
+        time.sleep(TIME_BETWEEN)
         
 if __name__ == '__main__':
     main()
