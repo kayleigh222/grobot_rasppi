45013d216de459d030e075c182694b7930e0db58
u7284920 <u7284920@anu.edu.au>
2025-02-17 09:16:47 +1100

    trying with configuration to remove brg warning

--- barcode_scan.py
+++ barcode_scan.py
@@ -1,16 +1,35 @@
 import cv2
+import numpy as np
 from picamera2 import Picamera2
 
-picam2 = Picamera2()
-picam2.start()
-while True:
-    image = picam2.capture_array()
-    cv2.imshow("Frame", image)
-    if(cv2.waitKey(1) == ord("q")):
-        cv2.imwrite("test_frame.png", image)
-        break
+cam = Picamera2()
+height = 480
+width = 640
+middle = (int(width / 2), int(height / 2))
+cam.configure(cam.create_video_configuration(main={"format": 'RGB888', "size": (width, height)}))
 
-cv2.destroyAllWindows()
+cam.start()
+
+while True:
+        frame = cam.capture_array()
+        cv2.circle(frame, middle, 10, (255, 0 , 255), -1)
+        cv2.imshow('f', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+# import cv2
+# from picamera2 import Picamera2
+
+# picam2 = Picamera2()
+# picam2.start()
+# while True:
+#     image = picam2.capture_array()
+#     cv2.imshow("Frame", image)
+#     if(cv2.waitKey(1) == ord("q")):
+#         cv2.imwrite("test_frame.png", image)
+#         break
+
+# cv2.destroyAllWindows()
 
 # import cv2
 # from picamera2 import Picamera2
