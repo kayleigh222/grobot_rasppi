383f62d8750e012eced734bda4f93390bcbab8b0
kayleigh222 <94883925+kayleigh222@users.noreply.github.com>
2025-03-16 17:02:57 +1100

    Update servo_motor_code.py

--- servo_motor_code.py
+++ servo_motor_code.py
@@ -2,7 +2,7 @@ import pigpio
 import time
 
 PWM_PIN = 18
-pi = pigpio.pi()  # Connect to pigpio daemon
+pi = pigpio.pi()  # Connect to pigpio daemon for PWM
 
 if not pi.connected:
     exit("pigpio is not running!")
@@ -20,42 +20,3 @@ time.sleep(2)
 print("Stopping PWM...")
 pi.set_servo_pulsewidth(PWM_PIN, 0)  # Stop signal
 pi.stop()
-
-
-# import RPi.GPIO as GPIO
-# import time
-
-# # Set GPIO mode
-# GPIO.setmode(GPIO.BCM)
-
-# # Define the servo pin
-# SERVO_PIN = 18
-
-# # Set up the pin as an output
-# GPIO.setup(SERVO_PIN, GPIO.OUT)
-
-# # Set PWM frequency to 50Hz (Standard for SG90 servos)
-# pwm = GPIO.PWM(SERVO_PIN, 50)
-
-# # Start PWM with 0-degree position
-# pwm.start(0)
-
-# def set_angle(angle):
-#     """ Move the servo to a specific angle (0 to 180). """
-#     duty_cycle = (angle / 18) + 2  # Convert angle to duty cycle
-#     pwm.ChangeDutyCycle(duty_cycle)
-#     time.sleep(0.5)  # Give the servo time to reach position
-#     pwm.ChangeDutyCycle(0)  # Prevent jitter
-
-# try:
-#     set_angle(90)
-    # time.sleep(0.5)  # Give the servo time to reach position
-    # set_angle(0)
-
-
-# except KeyboardInterrupt:
-#     print("\nExiting...")
-
-# # finally:
-# #     pwm.stop()
-# #     GPIO.cleanup()
