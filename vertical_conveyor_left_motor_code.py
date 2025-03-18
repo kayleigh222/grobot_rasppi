b6e1f4566d1ed8b70901514866c3e2d5445bd8ec
kayleigh222 <94883925+kayleigh222@users.noreply.github.com>
2025-03-18 15:57:55 +1100

    Update vertical_conveyor_left_motor_code.py

--- vertical_conveyor_left_motor_code.py
+++ vertical_conveyor_left_motor_code.py
@@ -1,23 +1,36 @@
 import RPi.GPIO as GPIO
 import time
 
-# Define GPIO pins based on new wiring
+# Define GPIO pins
 DIR_PIN = 7     # Direction control
 STEP_PIN = 8    # Step signal
 SLEEP_PIN = 24   # Sleep mode control
 RESET_PIN = 25   # Reset control
 
-# Setup GPIO
-GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR_PIN, GPIO.OUT)
-GPIO.setup(STEP_PIN, GPIO.OUT)
-GPIO.setup(SLEEP_PIN, GPIO.OUT)
-GPIO.setup(RESET_PIN, GPIO.OUT)
+def move_left_conveyor_up(steps):
+    move_stepper(steps, "CCW")
 
-# Enable driver by pulling SLEEP and RESET HIGH
-GPIO.output(SLEEP_PIN, GPIO.HIGH)
-GPIO.output(RESET_PIN, GPIO.HIGH)
+def move_left_conveyor_down(steps):
+    move_stepper(steps, "CW")
+
+def set_up_left_conveyor():
+    # Setup GPIO
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(DIR_PIN, GPIO.OUT)
+    GPIO.setup(STEP_PIN, GPIO.OUT)
+    GPIO.setup(SLEEP_PIN, GPIO.OUT)
+    GPIO.setup(RESET_PIN, GPIO.OUT)
+    
+    # Enable driver by pulling SLEEP and RESET HIGH
+    GPIO.output(SLEEP_PIN, GPIO.HIGH)
+    GPIO.output(RESET_PIN, GPIO.HIGH)
+
+def clean_up_left_conveyor():
+    # Disable driver by pulling SLEEP and RESET LOW
+    GPIO.output(SLEEP_PIN, GPIO.LOW)
+    GPIO.output(RESET_PIN, GPIO.LOW)
+    GPIO.cleanup()
 
 def move_stepper(steps, direction="CW", delay=0.001):
     if direction == "CW":
@@ -33,17 +46,3 @@ def move_stepper(steps, direction="CW", delay=0.001):
         time.sleep(delay)
         GPIO.output(STEP_PIN, GPIO.LOW)
         time.sleep(delay)
-
-try:
-    while True:
-        print("Moving Forward")
-        move_stepper(800, "CW")  # Move 200 steps clockwise
-        time.sleep(1)
-        
-        print("Moving Backward")
-        move_stepper(800, "CCW") # Move 200 steps counterclockwise
-        time.sleep(1)
-
-except KeyboardInterrupt:
-    print("Stopping motor and cleaning up GPIO")
-    GPIO.cleanup()
