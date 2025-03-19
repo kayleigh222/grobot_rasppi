27fcdead083ea5c2872faa1c9c115f4d0cf0c85c
kayleigh222 <94883925+kayleigh222@users.noreply.github.com>
2025-03-19 11:10:19 +1100

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -1,5 +1,5 @@
 import json
-from image_analysis import barcodes_divided_into_conveyors
+from image_analysis import top_barcode_right_conveyor, top_barcode_left_conveyor
 import os
 import cv2
 from vertical_conveyor_left_motor_code import move_left_conveyor_up, move_left_conveyor_down, set_up_left_conveyor, clean_up_left_conveyor
@@ -21,21 +21,41 @@ def load_variables():
     except (FileNotFoundError, json.JSONDecodeError):
         return {}  # Return empty dict if file doesn't exist or is corrupted
 
-def calibrate_right_conveyor_motor():  # to use, put one barcode on left conveyor somewhere in the middle
+def calibrate_right_conveyor_motor(num_steps_to_test=100):  # to use, put one barcode on left conveyor somewhere in the middle
   image_path = "captured_image.jpg"
   os.system(f"rpicam-still --output {image_path} --nopreview") # capture image without displaying preview
   image = cv2.imread(image_path) # read the captured image with opencv
-  left_conveyor_barcodes, right_conveyor_barcodes = barcodes_divided_into_conveyors(image)
-  if right_conveyor_barcodes:
-    # Find the barcode with the maximum x-coordinate in the right conveyor
-    top_barcode_right_conveyor = max(right_conveyor_barcodes, key=lambda point: point[0])
-  else:
-    # Handle the case where there are no barcodes in the right conveyor
-    top_barcode_right_conveyor = None  # or some default value/message
-  print("Top barcode right conveyor:", top_barcode_right_conveyor)
-  
-  
-  data = {"right_conveyor_motor_pixels_per_step_up": 120, "right_conveyor_motor_pixels_per_step_down": 120}
+  top_barcode_right_conveyor_original = top_barcode_right_conveyor(image)
+
+  set_up_right_conveyor()
+
+  # move motor up
+  move_right_conveyor_up(num_steps_to_test)
+
+  # measure new position
+  os.system(f"rpicam-still --output {image_path} --nopreview") # capture image without displaying preview
+  image = cv2.imread(image_path) # read the captured image with opencv
+  top_barcode_right_conveyor_new = top_barcode_right_conveyor(image)
+  # calculate num pixels moved
+  pixels_moved = abs(top_barcode_right_conveyor_new[0] - top_barcode_right_conveyor_original[0])
+  pixels_moved_per_step_up = pixels_moved/num_steps_to_test
+
+  # prepare for downward test
+  top_barcode_right_conveyor_original = top_barcode_right_conveyor_new
+
+  # move motor down
+  move_right_conveyor_down(num_steps_to_test)
+
+  # measure new position
+  os.system(f"rpicam-still --output {image_path} --nopreview") # capture image without displaying preview
+  image = cv2.imread(image_path) # read the captured image with opencv
+  top_barcode_right_conveyor_new = top_barcode_right_conveyor(image)
+  # calculate num pixels moved
+  pixels_moved = abs(top_barcode_right_conveyor_new[0] - top_barcode_right_conveyor_original[0])
+  pixels_moved_per_step_down = pixels_moved/num_steps_to_test
+
+  # save new calibration variables
+  data = {"right_conveyor_motor_pixels_per_step_up": pixels_moved_per_step_up, "right_conveyor_motor_pixels_per_step_down": pixels_moved_per_step_down}
   save_variables(data)  # Save
 
 # loaded_data = load_variables()  # Load
