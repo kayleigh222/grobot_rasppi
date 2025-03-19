f02ba6061f90303fe9e8325e8dac4c955e005b83
kayleigh222 <kayleigh.sleath@qdx.co>
2025-03-19 21:04:09 +1100

    moving to top

--- rotate_plant_anticlockwise.py
+++ rotate_plant_anticlockwise.py
@@ -2,11 +2,14 @@ import os
 import cv2
 from image_analysis import find_top_and_bottom_of_conveyors, top_barcode_right_conveyor
 from calibration import calibrate_vertical_conveyor_motors, load_variables, LEFT_CONVEYOR_SPEED, RIGHT_CONVEYOR_SPEED
+from vertical_conveyor_left_motor_code import move_left_conveyor_up, move_left_conveyor_down, set_up_left_conveyor, clean_up_left_conveyor
+from vertical_conveyor_right_motor_code import move_right_conveyor_up, move_right_conveyor_down, set_up_right_conveyor, clean_up_right_conveyor
+
 
 # NOTE: have a record of how many plants there are i.e. how many barcodes are visible. therefore if a plant falls off will know because less barcodes visible and can send me a photo
 
 # calibrate conveyor motors
-calibrate_vertical_conveyor_motors()
+# calibrate_vertical_conveyor_motors()
 
 # simpler version - move plant from right to left conveyor (do an equivalent version to move plant from left to right conveyor)
 # step 1: check location of top plant on right conveyor (barcode in top left position) - note distance from top
@@ -23,7 +26,13 @@ print("Distance between: ", distance_from_top)
 
 calibration_variables = load_variables() 
 print(calibration_variables[LEFT_CONVEYOR_SPEED])  
-print(calibration_variables[RIGHT_CONVEYOR_SPEED])  
+print(calibration_variables[RIGHT_CONVEYOR_SPEED]) 
+
+steps_to_top = distance_from_top / calibration_variables[RIGHT_CONVEYOR_SPEED]
+set_up_right_conveyor()
+move_right_conveyor_up(steps_to_top)
+clean_up_right_conveyor()
+
 
 # step 2: rotate right conveyor until plant at top
 # step 3: check location of holder on left conveyor
