fb01eeed54e766f2aa82e47f6529e6572c863814
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-14 16:35:27 +1000

    testing servo motor

--- servo_motor_code.py
+++ servo_motor_code.py
@@ -1,13 +1,13 @@
 import pigpio
 import time
 
-PWM_PIN = 18
+PWM_PIN = 13
 pi = pigpio.pi()  # Connect to pigpio daemon for PWM
 
 if not pi.connected:
     exit("pigpio is not running!")
 
-print("Setting PWM on GPIO 18 (50Hz, 1.5ms pulse)...")
+print("Setting PWM on GPIO 13 (50Hz, 1.5ms pulse)...")
 pi.set_PWM_frequency(PWM_PIN, 50)  # 50Hz servo frequency
 pi.set_servo_pulsewidth(PWM_PIN, 1500)  # Move to center position (90 degrees)
 
