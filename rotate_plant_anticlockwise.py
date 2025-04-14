7018c11ac35809344fd58888edbce2f8d2f54900
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-15 08:31:46 +1000

    saving gray to debug, and reducing corner quality level on right contour

--- rotate_plant_anticlockwise.py
+++ rotate_plant_anticlockwise.py
@@ -141,7 +141,8 @@ top_holder_right_contour = cv2.approxPolyDP(top_holder_right_contour, 0.01 * cv2
 image_with_right_contour = np.zeros_like(image)
 cv2.drawContours(image_with_right_contour, [top_holder_right_contour], -1, (255, 255, 255), 1)
 right_gray = cv2.cvtColor(image_with_right_contour, cv2.COLOR_BGR2GRAY)
-corners_right = cv2.goodFeaturesToTrack(right_gray, maxCorners=16, qualityLevel=0.05, minDistance=10)
+cv2.imwrite('right_gray.jpg', right_gray) # save the gray image for debugging
+corners_right = cv2.goodFeaturesToTrack(right_gray, maxCorners=16, qualityLevel=0.03, minDistance=10)
 corners_right = np.intp(corners_right)
 cv2.drawContours(image_with_contours, [top_holder_right_contour], -1, (255, 0, 0), 1) # draw right holder contour in blue
 
@@ -156,6 +157,7 @@ top_holder_left_contour = cv2.approxPolyDP(top_holder_left_contour, 0.01 * cv2.a
 image_with_left_contour = np.zeros_like(image)
 cv2.drawContours(image_with_left_contour, [top_holder_left_contour], -1, (255, 255, 255), 1)
 left_gray = cv2.cvtColor(image_with_left_contour, cv2.COLOR_BGR2GRAY)
+cv2.imwrite('left_gray.jpg', left_gray) # save the gray image for debugging
 corners_left = cv2.goodFeaturesToTrack(left_gray, maxCorners=8, qualityLevel=0.01, minDistance=20)
 corners_left = np.intp(corners_left)
 cv2.drawContours(image_with_contours, [top_holder_left_contour], -1, (0, 0, 255), 1) # draw left holder contour in red
