3685de2e2758872f9f6f94168e69a1d414a8439b
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-14 11:14:23 +1000

    increase distance to push over threshold

--- rotate_plant_anticlockwise.py
+++ rotate_plant_anticlockwise.py
@@ -155,7 +155,7 @@ print('finished moving holders together')
 
 # ------- ROTATE TOP CONVEYOR TO SLIDE TRAY ACROSS -----------
 set_up_top_conveyor()
-additional_distance_to_push_tray_across_threshold = 50 # (conveyors_right - conveyors_left) // 20 # move an extra quarter of a conveyor across threshold
+additional_distance_to_push_tray_across_threshold = 100 # (conveyors_right - conveyors_left) // 20 # move an extra quarter of a conveyor across threshold
 distance_from_target = top_conveyor_leg_top_left_y - (conveyor_threshold - additional_distance_to_push_tray_across_threshold)
 # draw a horizontal line at conveyor_threshold
 cv2.line(image, (0, conveyor_threshold), (image.shape[1], conveyor_threshold), (0, 255, 0), 2)  # Green line
@@ -163,7 +163,7 @@ cv2.line(image, (0, conveyor_threshold), (image.shape[1], conveyor_threshold), (
 cv2.line(image, (0, top_conveyor_leg_top_left_y), (image.shape[1], top_conveyor_leg_top_left_y), (0, 0, 255), 2)  # Red line
 cv2.imwrite("before_move_top_conveyor.jpg", image)
 
-while(abs(distance_from_target) > DISTANCE_BELOW_TARGET_HOLDER_TO_SLIDE_ACROSS):
+while(abs(distance_from_target) > 50):
     steps_to_take = int(pid_control(distance_from_target, Kp=(1/calibration_variables[TOP_CONVEYOR_SPEED_FORWARD])))
     if(steps_to_take == 0):
         print("No steps to take")
@@ -179,7 +179,7 @@ while(abs(distance_from_target) > DISTANCE_BELOW_TARGET_HOLDER_TO_SLIDE_ACROSS):
     top_conveyor_leg_top_left_x, top_conveyor_leg_top_left_y = find_leg_top_conveyor(image)
     distance_from_target = top_conveyor_leg_top_left_y - (conveyor_threshold - additional_distance_to_push_tray_across_threshold)
 
-    print("Distance between from top conveyor target: ", distance_from_target)
+    print("Distance from top conveyor target: ", distance_from_target)
 
 print('finished moving top conveyor to target')
 
