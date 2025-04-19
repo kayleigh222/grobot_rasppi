275289f23cba34ba9ba2191fad10fdb0c0f7b680
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-19 17:52:47 +1000

    fix corner indexing

--- rotate_plant_anticlockwise.py
+++ rotate_plant_anticlockwise.py
@@ -208,16 +208,15 @@ try:
     top_left_corner_right_holder = min(corners_right, key=lambda pt: pt[0] - pt[1])
     del corners_right
 
-    target_x_value = top_left_corner_right_holder[0][0]
+    target_x_value = top_left_corner_right_holder[0]
     print("Target x value: ", target_x_value)
-    print("Bottom left corner left holder: ", bottom_left_corner_left_holder[0][0])
 
     # visualize positions on image
-    cv2.circle(image, (bottom_left_corner_left_holder[0][0], bottom_left_corner_left_holder[0][1]), 10, (0, 255, 255), -1)  # Yellow circle for left edge
-    cv2.circle(image, (top_left_corner_right_holder[0][0], top_left_corner_right_holder[0][1]), 10, (0, 255, 255), -1)  # Yellow circle for right edge
+    cv2.circle(image, (bottom_left_corner_left_holder[0], bottom_left_corner_left_holder[1]), 10, (0, 255, 255), -1)  # Yellow circle for left edge
+    cv2.circle(image, (top_left_corner_right_holder[0], top_left_corner_right_holder[1]), 10, (0, 255, 255), -1)  # Yellow circle for right edge
     cv2.imwrite("image_before_move_left_holder.jpg", image)
 
-    distance_below_target = target_x_value - bottom_left_corner_left_holder[0][0]
+    distance_below_target = target_x_value - bottom_left_corner_left_holder[0]
 
     print("Distance between holders: ", distance_below_target)
 
