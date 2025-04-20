edc9e01adbae6642a2940549e25c1db0551a57ee
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-20 19:19:54 +1000

    adjust finding bottom left corner

--- image_analysis.py
+++ image_analysis.py
@@ -123,7 +123,7 @@ def find_left_and_right_of_conveyors(image):
 # ----------- HOLDER DETECTION -------------
 def get_bottom_left_corner(corners):
     # takes an array of corner coordinates and returns the bottom leftmost
-    return min(corners, key=lambda pt: 0.4*pt[0] - 0.6*pt[1])
+    return min(corners, key=lambda pt: 0.2*pt[0] - 0.8*pt[1])
 
 def get_top_left_corner(corners):
     # takes an array of corner coordinates and returns the top leftmost
