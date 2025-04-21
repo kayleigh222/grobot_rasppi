5462d51a7373f374451c8a40fd4ef2750826c407
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-22 07:34:23 +1000

    correct top left

--- image_analysis.py
+++ image_analysis.py
@@ -458,7 +458,7 @@ def find_qrcodes(image):
             data = qr.data.decode("utf-8")
             x, y, w, h = qr.rect
             center = (x + w / 2, y + h / 2)
-            top_left = (x, y)
+            top_left = (x+w, y)
             # draw a dot at top left of barcode
             cv2.circle(image, top_left, 5, (0, 255, 0), -1)  # Green circle
             qrcode_info.append((data, center, top_left))
