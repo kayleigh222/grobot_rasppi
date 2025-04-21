45b4f59fef39ea3d079e4bf4cbe3c7499052f30f
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-21 22:01:19 +1000

    take new image

--- image_analysis.py
+++ image_analysis.py
@@ -142,10 +142,10 @@ def find_borders_of_conveyors(image):
     # get the contours of the mask
     contours = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     
-    cv2.drawContours(image, contours, -1, (255, 0, 0), 3)
-    cv2.imwrite('image_with_conveyor_contours_before_size_filtering.jpg', image)  # Save the image with the contours for debugging
+    # cv2.drawContours(image, contours, -1, (255, 0, 0), 3)
+    # cv2.imwrite('image_with_conveyor_contours_before_size_filtering.jpg', image)  # Save the image with the contours for debugging
 
-    min_area = 50000 # minimum number of dark pixels for a contour to be considered part of the conveyor
+    min_area = 60000 # minimum number of dark pixels for a contour to be considered part of the conveyor
     
     # filter the contours to only ones with the minimum area
     contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
@@ -479,8 +479,8 @@ def find_qrcodes(image):
 
 if __name__ == "__main__":
     gc.collect()  # Run garbage collection to free up memory
-    # image = capture_image()
-    image = cv2.imread('captured_image.jpg')
+    image = capture_image()
+    # image = cv2.imread('captured_image.jpg')
     print("Image loaded successfully.")
     holders = find_holders(image)
     print(f"Number of holders found: {len(holders)}")
