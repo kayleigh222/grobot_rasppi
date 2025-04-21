fe36a2482a3dacc83b1258139c31b0a5bfbc02b8
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-21 20:42:50 +1000

    decrease min area

--- image_analysis.py
+++ image_analysis.py
@@ -134,7 +134,7 @@ def find_left_and_right_of_conveyors(image):
     # get the contours of the mask
     contours = cv2.findContours(binary_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     
-    min_area = 100000 # minimum number of dark pixels for a contour to be considered part of the conveyor
+    min_area = 50000 # minimum number of dark pixels for a contour to be considered part of the conveyor
     
     # filter the contours to only ones with the minimum area
     contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
