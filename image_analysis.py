89105190185639ce1f0981d41fe5ee607bcc0d6f
kayleigh222 <kayleigh.sleath@qdx.co>
2025-04-21 21:42:35 +1000

    increase min intensity threshold

--- image_analysis.py
+++ image_analysis.py
@@ -135,7 +135,7 @@ def find_borders_of_conveyors(image):
     # equalize the image
     equalized = cv2.equalizeHist(gray)
     cv2.imwrite('equalized_conveyor_image.jpg', equalized)  # Save the equalized image for debugging
-    _, binary_mask = cv2.threshold(equalized, 40, 255, cv2.THRESH_BINARY_INV) # changed intesnity from 50
+    _, binary_mask = cv2.threshold(equalized, 45, 255, cv2.THRESH_BINARY_INV) # changed intesnity from 50
 
     # binary_mask = np.where(gray < 50, 1, 0) # Create a binary mask where intensity < 50 is set to 1, and others are set to 0
     # get the contours of the mask
