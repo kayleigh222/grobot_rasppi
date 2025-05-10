6937996646d509334a6e44a9c71bfab65f9e6d40
kayleigh222 <kayleigh.sleath@qdx.co>
2025-05-10 14:53:46 +1000

    return watering code

--- watering_code.py
+++ watering_code.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 
-from vertical_conveyor_right_motor_code import clean_up_right_conveyor, move_right_conveyor, set_up_right_conveyor
+# from vertical_conveyor_right_motor_code import clean_up_right_conveyor, move_right_conveyor, set_up_right_conveyor
 
 # turns on the watering system for X seconds. to be scheduled by a cronjob.
 
@@ -9,7 +9,7 @@ WATERING_TIME = 10 # seconds to turn on misting system
 
 # compressor relay setup
 RELAY_PIN = 17
-set_up_right_conveyor()
+# set_up_right_conveyor()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
 
@@ -18,6 +18,6 @@ print("Watering system activated")
 time.sleep(WATERING_TIME)
 GPIO.output(RELAY_PIN, GPIO.HIGH)
 print("Watering system deactivated")
-time.sleep(5)
-move_right_conveyor(1500) # Move down 1000 steps
-clean_up_right_conveyor()
+# time.sleep(5)
+# move_right_conveyor(1500) # Move down 1000 steps
+# clean_up_right_conveyor()
